2eb0a495721ef1c7c300a2d5c9b3465af6c4a84f
Saulo <saulopineda@gmail.com>
2018-09-06 17:41:26 -0500

    Primera versión
    Primera versión

---
+++ Ejercicio/Ejercicio/src/Entidades/Persona.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Saulo Pineda
+ */
+public class Persona {
+    private String Nombre;
+    private String Apellido;
+    private double Peso;
+    private double Estatura;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public double getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(double Peso) {
+        this.Peso = Peso;
+    }
+
+    public double getEstatura() {
+        return Estatura;
+    }
+
+    public void setEstatura(double Estatura) {
+        this.Estatura = Estatura;
+    }
+    
+}
